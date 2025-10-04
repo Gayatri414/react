7ce1ddd2db29e847dcd39383fd95934615f1bb00
Gayatri414 <gayatrigade7949@gmail.com>
2025-10-04 12:53:15 +0530

    fourth project of react

---
+++ reactRouter/src/Layout.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header/Header.jsx'
+import Footer from "./components/Header/Footer/Footer.jsx";
+
+
+import{Outlet} from 'react-router-dom'
+
+const Layout = () => {
+  return (
+    
+      <>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+
+      
+      </>
+    
+  )
+}
+
+export default Layout
